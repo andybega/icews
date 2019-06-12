afe17f8e2f34671070a4cdbccfd6f9078c85ebef
Andreas Beger <adbeger@gmail.com>
2019-06-12 16:23:03 +0300

    change local state getter to new format

--- R/state_local_files.R
+++ R/state_local_files.R
@@ -5,9 +5,10 @@
 #' @export
 get_local_state <- function(raw_file_dir = find_raw()) {
   files <- basename(list_local_files(raw_file_dir))
-  state <- parse_label(files)
-  state$label <- state$is_data <- NULL
-  colnames(state) <- paste0("local_", colnames(state))
+
+  state <- tibble(
+    local_file_name = files
+  )
   state
 }
 
@@ -22,15 +23,26 @@ get_local_state <- function(raw_file_dir = find_raw()) {
 list_local_files <- function(raw_file_dir = find_raw(), full_names = TRUE) {
   o <- dir(raw_file_dir, full.names = full_names)
   # check it's all events.YYYY....tab or YYYYMMDD-icews-events.zip files
-  good1 <- grepl("^events\\.[0-9]{4}\\.[0-9a-z]+\\.tab$", basename(o))
-  good2 <- grepl("^[0-9]{8}[0-9a-z\\-]+\\.tab$", basename(o))
-  offending <- !good1 & !good2
-  if (any(offending)) {
-    ff <- paste0("  ", basename(o)[offending], collapse = "  \n")
+  is_df <- is_data_file(o)
+  if (any(!is_df)) {
+    ff <- paste0("  ", basename(o)[!is_df], collapse = "  \n")
     msg <- sprintf("unexpected non-data file(s) found in '%s':", raw_file_dir)
     msg <- paste0(c(msg, ff), collapse = "\n")
     stop(msg)
   }
   o
 }
+
+
+is_data_file <- function(x) {
+  # check it's all events.YYYY....tab or YYYYMMDD-icews-events.zip files
+  good1 <- grepl("^events\\.[0-9]{4}\\.[0-9a-z]+\\.tab$", basename(x))
+  good2 <- grepl("^[0-9]{8}[0-9a-z\\-]+\\.tab$", basename(x))
+
+  good1 | good2
+}
+
+
+
+
 
